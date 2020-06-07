a8217504d6245aca71b10d0dd911cbd66fd36692
Anthony Horton <anthorton247@gmail.com>
2020-06-07 16:06:09 -0400

    Week1 ToDO List

---
+++ Week1/Components/List.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function List() {
+    return (
+        <div>
+            <ol>
+                <li>
+                One
+                </li>
+                <li>
+                Two
+                </li>
+                <li>
+                Three
+                </li>
+            </ol>
+        </div>
+    )
+}
+
+export default List
