a1578b0c70caddfbaea8b774584208f243205283
Anthony Horton <anthorton247@gmail.com>
2020-06-25 01:35:58 -0400

    DJ Event

--- Week3/dj-app/src/Components/DJBox.js
+++ Week3/dj-app/src/Components/DJBox.js
@@ -2,13 +2,12 @@ import React from "react"
 
 function DJBox(props) {
     const style = {
-        width: 100,
-        height: 100,
-        color: props.color,
+        height: 300,
+        background: props.color,
         gridColumn: "span 1"
     }
     return (
-        <div stlye={stlye}>
+        <div style={style}>
         </div>
     )
 }
